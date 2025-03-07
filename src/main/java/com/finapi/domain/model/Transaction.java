4c748b1266dd6005fa261e7387f7bdb8cd9951d4
Anag0es <analuisa.goes2004@gmail.com>
2025-03-07 15:26:29 -0300

    fix: Transaction getter e setter

--- src/main/java/com/finapi/domain/model/Transaction.java
+++ src/main/java/com/finapi/domain/model/Transaction.java
@@ -20,4 +20,117 @@ public class Transaction {
     private User user;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    public Transaction() {}
+
+    public Transaction(Long id, TransactionType type, BigDecimal amount, LocalDateTime transactionDate, String description, PeriodicityType periodicity, boolean isAvailableBalance, BankAccount bankAccount, Card card, User user, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.type = type;
+        this.amount = amount;
+        this.transactionDate = transactionDate;
+        this.description = description;
+        this.periodicity = periodicity;
+        this.isAvailableBalance = isAvailableBalance;
+        this.bankAccount = bankAccount;
+        this.card = card;
+        this.user = user;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(LocalDateTime transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public PeriodicityType getPeriodicity() {
+        return periodicity;
+    }
+
+    public void setPeriodicity(PeriodicityType periodicity) {
+        this.periodicity = periodicity;
+    }
+
+    public boolean isAvailableBalance() {
+        return isAvailableBalance;
+    }
+
+    public void setAvailableBalance(boolean availableBalance) {
+        isAvailableBalance = availableBalance;
+    }
+
+    public BankAccount getBankAccount() {
+        return bankAccount;
+    }
+
+    public void setBankAccount(BankAccount bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
