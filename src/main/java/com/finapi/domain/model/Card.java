1b95295e4bf06b2894cf9fc54ec3b288fd35e266
Anag0es <analuisa.goes2004@gmail.com>
2025-03-24 16:45:30 -0300

    feat: Refactor Card class to use UUID for id and LocalDateTime for expiration

--- src/main/java/com/finapi/domain/model/Card.java
+++ src/main/java/com/finapi/domain/model/Card.java
@@ -6,18 +6,123 @@ import com.finapi.domain.enums.Status;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.UUID;
 
 public class Card {
 
-    private Long id;
+    private UUID id;
     private String name;
     private CardType type;
     private String number;
-    private Date expiration;
+    private LocalDateTime expiration;
     private BigDecimal limitAmount;
     private String band;
     private User user;
     private Status status;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    public Card() {}
+
+    public Card(UUID id, String name, CardType type, String number, LocalDateTime expiration, BigDecimal limitAmount, String band, User user, Status status, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.number = number;
+        this.expiration = expiration;
+        this.limitAmount = limitAmount;
+        this.band = band;
+        this.user = user;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CardType getType() {
+        return type;
+    }
+
+    public void setType(CardType type) {
+        this.type = type;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public LocalDateTime getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(LocalDateTime expiration) {
+        this.expiration = expiration;
+    }
+
+    public BigDecimal getLimitAmount() {
+        return limitAmount;
+    }
+
+    public void setLimitAmount(BigDecimal limitAmount) {
+        this.limitAmount = limitAmount;
+    }
+
+    public String getBand() {
+        return band;
+    }
+
+    public void setBand(String band) {
+        this.band = band;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
