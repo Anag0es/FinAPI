a8b0c7d43a180710927af181f82354d409950a01
Anag0es <analuisa.goes2004@gmail.com>
2025-03-24 19:29:27 -0300

    refactor: Remove unused import in CreateCardUseCase interface

--- src/main/java/com/finapi/application/port/in/card/CreateCardUseCase.java
+++ src/main/java/com/finapi/application/port/in/card/CreateCardUseCase.java
@@ -1,7 +1,6 @@
 package com.finapi.application.port.in.card;
 
 import com.finapi.application.dto.request.CreateCardDTO;
-import com.finapi.application.dto.response.ResponseCardDTO;
 import com.finapi.domain.model.Card;
 
 public interface CreateCardUseCase {
