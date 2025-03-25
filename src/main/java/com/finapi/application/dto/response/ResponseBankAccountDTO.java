6026f7914e90a2b7f6545d4786aea9f697f6868c
Anag0es <analuisa.goes2004@gmail.com>
2025-03-25 11:11:18 -0300

    feat: Add CreateBankAccountDTO and ResponseBankAccountDTO for bank account management

---
+++ src/main/java/com/finapi/application/dto/response/ResponseBankAccountDTO.java
@@ -0,0 +1,95 @@
+package com.finapi.application.dto.response;
+
+import com.finapi.domain.enums.Status;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class ResponseBankAccountDTO {
+
+    private String name;
+    private String type;
+    private String bank;
+    private String branch;
+    private String accountNumber;
+    private BigDecimal currentBalance;
+    private Status status;
+    private UUID userId;
+
+    public ResponseBankAccountDTO() {}
+
+    public ResponseBankAccountDTO(String name, String type, String bank, String branch, String accountNumber, BigDecimal currentBalance, Status status, UUID userId) {
+        this.name = name;
+        this.type = type;
+        this.bank = bank;
+        this.branch = branch;
+        this.accountNumber = accountNumber;
+        this.currentBalance = currentBalance;
+        this.status = status;
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBank() {
+        return bank;
+    }
+
+    public void setBank(String bank) {
+        this.bank = bank;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public BigDecimal getCurrentBalance() {
+        return currentBalance;
+    }
+
+    public void setCurrentBalance(BigDecimal currentBalance) {
+        this.currentBalance = currentBalance;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+}
